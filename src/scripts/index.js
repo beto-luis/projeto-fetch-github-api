5eda67f2a11beb051865c795a20fdfbf31ac1051
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-10-13 17:26:36 -0300

    Validações finais realizadas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,14 +4,10 @@ import { getRepositories } from '/src/scripts/services/repositories.js'
 import { user } from '/src/scripts/objects/user.js'
 import { screen } from '/src/scripts/objects/screen.js'
 
-
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
-     if(userName.lenght === 0){
-         alert('Preencha o campo com o nome do usuário do GitHub')
-         return 
-     }
-    getUserProfile(userName)
+    if(validadeEmptyInput(userName)) return
+    getUserData(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
@@ -20,15 +16,29 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const isEnterKeyPressed = key === 13
 
     if(isEnterKeyPressed){
+        if(validadeEmptyInput(userName)) return
         getUserData(userName)
     }
 })
 
+function validadeEmptyInput(userName){
+    if(userName.length === 0){
+        alert('Preencha o campo com o nome do usuário do GitHub')
+        return true
+    }
+}
+
 async function getUserData(userName){
 
     const userResponse = await getUser(userName)
-    const repositoriesResponse = await getRepositories(userName
-        )
+
+    if(userResponse.message === 'Not Found'){
+        screen.renderNotFound()
+        return
+    }
+
+    const repositoriesResponse = await getRepositories(userName)
+
     user.setInfo(userResponse)
     user.setRepositories(repositoriesResponse)
 
--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -4,13 +4,13 @@ const screen = {
         this.userProfile.innerHTML = `<div class="info">
                             <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
                             <div class="data">
-                                <h1>${user.name ?? 'Não possui nome cadastrada 😥'}</h1>
-                                <p>${user.bio ?? 'Não possui bio cadastrada 😥'}</p>
+                                <h1>${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
+                                <p>${user.bio ?? 'Não possui bio cadastrado 😥'}</p>
                             </div>
                         </div>`
 
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}"target="_blank">${repo.name}</a></li>`)
+        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
 
         if(user.repositories.length > 0){
             this.userProfile.innerHTML +=  `<div class="repositories section">
@@ -18,6 +18,9 @@ const screen = {
                                                 <ul>${repositoriesItens}</ul>
                                             </div>`
         }
+    },
+    renderNotFound(){
+        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
     }
 }
 
